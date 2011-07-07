01aaa5f0870ee6857c05806863b0fb4018de31dd
Kris Craig <kris.craig@gmail.com>
2011-07-06 20:03:41 -0700

    Changed my mind on the constructor setup.  Passing a single array with $varname => $value pairs makes a lot more sense in my opinion.

--- phpmeow.class.php
+++ phpmeow.class.php
@@ -2,16 +2,19 @@
 
 class phpmeow
 {
-	/* If passing args, arg 0 must be an array containing the variable names to which to assign each subsequent arg value.  --Kris */
+	/* Each arg must be an array in $varname => $value format.  --Kris */
 	public function __construct()
 	{
 		$args = func_get_args();
 		
-		if ( is_array( $args[0] ) && count( $args ) - 1 == count( $args[0] ) && count( $args ) > 1 )
+		foreach ( $args as $arr )
 		{
-			foreach ( $args[0] as $arkey => $varname )
+			if ( is_array( $arr ) && !empty( $arr ) )
 			{
-				$this->$varname = $args[$arkey + 1];
+				foreach ( $arr as $varname => $value )
+				{
+					$this->$varname = $value;
+				}
 			}
 		}
 	}
