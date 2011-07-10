92c3eaa5341e2f584b65a0c3e282fa8e340f881e
Kris Craig <kris.craig@gmail.com>
2011-07-10 01:10:37 -0700

    The basics.

--- security.phpmeow.class.php
+++ security.phpmeow.class.php
@@ -7,8 +7,14 @@ class phpmeow_security
 	{
 		if ( !isset( $_SESSION["phpmeow_attempts"] ) )
 		{
-			
+			$_SESSION["phpmeow_attempts"] = 0;
+			$_SESSION["phpmeow_failed_attempts"] = 0;
+			$_SESSION["phpmeow_last_attempt"] = 0;
+			$_SESSION["phpmeow_last_failed_attempt"] = 0;
+			$_SESSION["phpmeow_attempts_log"] = array();  // Indexed by timestamp, contains array of data.  --Kris
 		}
+		
+		// TODO - Load ipban
 	}
 	
 	/* Load ipban configuration file.  --Kris */
@@ -26,6 +32,9 @@ class phpmeow_security
 	/* Track a failed attempt and react accordingly.  --Kris */
 	function track_failure()
 	{
+		$_SESSION["phpmeow_failed_attempts"]++;
+		$_SESSION["phpmeow_last_failed_attempt"] = time();
 		
+		// TODO - ipban stuff, react
 	}
 }
